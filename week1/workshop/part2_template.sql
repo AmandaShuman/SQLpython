56af8729e8e2252602ae09a628ff5dac1fd0e8d2
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-09-25 22:31:59 -0400

    part 2 sql file for workshop 1

--- week1/workshop/part2_template.sql
+++ week1/workshop/part2_template.sql
@@ -18,81 +18,41 @@ SET default_with_oids = false;
 --- CREATE tables
 ---
 
-CREATE TABLE products (
-    product_id SERIAL,
-    product_name TEXT NOT NULL,
-    discontinued INT NOT NULL,
-    supplier_id INT,
-    category_id INT,
-    PRIMARY KEY(product_id)
-);
-
-
 CREATE TABLE categories (
     category_id SERIAL,
     category_name TEXT NOT NULL,
     category_description TEXT,
     picture TEXT,
     PRIMARY KEY(category_id)
-);
-
--- TODO create more tables here...
-
-CREATE TABLE customers (
-    customer_id SERIAL,
-    customer_name TEXT NOT NULL,
-    PRIMARY KEY(customer_id)
-);
-
-CREATE TABLE orders (
-    order_id SERIAL,
-    order_date timestamp,
-    PRIMARY KEY(order_id)
-);
-
-CREATE TABLE shippers (
-    shipper_id SERIAL,
-    shipper_name TEXT NOT NULL,
-    shipper_phone TEXT,
-    PRIMARY KEY(shipper_id)
-);
-
-CREATE TABLE employees (
-    employee_id SERIAL,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    repports_to TEXT,
-    territory_id INT,
-    PRIMARY KEY(employee_id)
+    /* doesn't need product_id */
 );
 
 CREATE TABLE suppliers (
     supplier_id SERIAL,
     supplier_name TEXT NOT NULL,
-    PRIMARY KEY(supplier_id)
+    PRIMARY KEY(supplier_id),
+    product_id INT NOT NULL /* because I decided supplier must have at least 1 product */
 );
 
-CREATE TABLE territories (
-    territory_id SERIAL,
-    territory_description TEXT NOT NULL,
-    PRIMARY KEY(territory_id)
+CREATE TABLE products (
+    product_id SERIAL,
+    product_name TEXT NOT NULL,
+    discontinued BOOLEAN NOT NULL, /* can only be 0 or 1 */
+    PRIMARY KEY(product_id),
+    supplier_id INT,
+    category_id INT
 );
 
-CREATE TABLE regions (
-    region_id SERIAL,
-    region_description TEXT,
-    territory_id INT,
-    PRIMARY KEY(region_id) 
+CREATE TABLE orders (
+    order_id SERIAL,
+    order_date timestamp,
+    PRIMARY KEY(order_id),
+    customer_id INT,
+    shipper_id INT,
+    employee_id INT
 );
 
-CREATE TABLE states (
-    state_id SERIAL,
-    state_name TEXT,
-    state_abbrev CHARACTER(2),
-    PRIMARY KEY(state_id)
-);
-
-CREATE TABLE products_orders (
+CREATE TABLE products_orders ( /* how are the 2 bold lines handled? */
     product_id INTEGER NOT NULL,
     order_id INTEGER NOT NULL,
     PRIMARY KEY(product_id, order_id),
@@ -100,10 +60,35 @@ CREATE TABLE products_orders (
     discount NUMERIC NOT NULL
 );
 
-CREATE TABLE region_state (
-    region_id INTEGER NOT NULL,
-    state_id INTEGER NOT NULL,
-    PRIMARY KEY(region_id, state_id)
+CREATE TABLE customers (
+    customer_id SERIAL,
+    customer_name TEXT NOT NULL,
+    PRIMARY KEY(customer_id)
+    /* doesn't need order_id */
+);
+
+CREATE TABLE shippers (
+    shipper_id SERIAL,
+    shipper_name TEXT NOT NULL,
+    shipper_phone TEXT,
+    PRIMARY KEY(shipper_id)
+    /* doesnt need order_id */
+);
+
+CREATE TABLE employees (
+    employee_id SERIAL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    PRIMARY KEY(employee_id),
+    reports_to TEXT, /* should this be an employee id? */
+    order_id INT NOT NULL
+);
+
+CREATE TABLE territories (
+    territory_id SERIAL,
+    territory_description TEXT NOT NULL,
+    PRIMARY KEY(territory_id),
+    region_id INT NOT NULL
 );
 
 CREATE TABLE employees_territories (
@@ -112,23 +97,34 @@ CREATE TABLE employees_territories (
     PRIMARY KEY(employee_id, territory_id)
 );
 
+CREATE TABLE regions (
+    region_id SERIAL,
+    region_description TEXT,
+    PRIMARY KEY(region_id),
+    territory_id INT NOT NULL
+);
+
+CREATE TABLE states (
+    state_id SERIAL,
+    state_name TEXT,
+    state_abbrev CHARACTER(2) NOT NULL,
+    PRIMARY KEY(state_id)
+);
 
 ---
 --- Add foreign key constraints
 ---
 
+/* one to many relationships */
 ALTER TABLE products
 ADD CONSTRAINT fk_products_categories 
 FOREIGN KEY (category_id) 
 REFERENCES categories;
 
-
--- TODO create more constraints here...
-
-ALTER TABLE customers
-ADD CONSTRAINT fk_customers_orders
-FOREIGN KEY (order_id)
-REFERENCES orders;
+ALTER TABLE products
+ADD CONSTRAINT fk_products_suppliers
+FOREIGN KEY (supplier_id)
+REFERENCES suppliers;
 
 ALTER TABLE orders
 ADD CONSTRAINT fk_orders_customers
@@ -145,39 +141,44 @@ ADD CONSTRAINT fk_orders_employees
 FOREIGN KEY (employee_id)
 REFERENCES employees;
 
-ALTER TABLE employees
-ADD CONSTRAINT fk_employees_territories
-FOREIGN KEY (territory_id)
-REFERENCES territories;
-
-ALTER TABLE products
-ADD CONSTRAINT fk_products_suppliers
-FOREIGN KEY (supplier_id)
-REFERENCES suppliers;
-
-ALTER TABLE products
-ADD CONSTRAINT fk_products_orders
-FOREIGN KEY (order_id)
-REFERENCES orders;
-
 ALTER TABLE territories
 ADD CONSTRAINT fk_territories_region
 FOREIGN KEY (region_id)
 REFERENCES regions;
 
-ALTER TABLE territories
-ADD CONSTRAINT fk_territories_employees
+/* altering hybrid tables for many:many relationships */
+ALTER TABLE products_orders
+ADD CONSTRAINT fk_products_orders_products
+FOREIGN KEY (product_id)
+REFERENCES products;
+
+ALTER TABLE products_orders
+ADD CONSTRAINT fk_products_orders_orders
+FOREIGN KEY (order_id)
+REFERENCES orders;
+
+ALTER TABLE employees_territories
+ADD CONSTRAINT fk_employees_territories_employees
 FOREIGN KEY (employee_id)
 REFERENCES employees;
+
+ALTER TABLE employees_territories
+ADD CONSTRAINT fk_employees_territories_territories
+FOREIGN KEY (territory_id)
+REFERENCES territories;
+
+/* for participates 1 or more times */
+ALTER TABLE suppliers
+ADD CONSTRAINT fk_suppliers_products
+FOREIGN KEY (product_id)
+REFERENCES products;
+
+ALTER TABLE employees
+ADD CONSTRAINT fk_employees_orders
+FOREIGN KEY (order_id)
+REFERENCES orders;
 
 ALTER TABLE regions
 ADD CONSTRAINT fk_regions_territories
 FOREIGN KEY (territory_id)
 REFERENCES territories;
-
-ALTER TABLE states
-ADD CONSTRAINT fk_state_regions
-FOREIGN KEY (region_id)
-REFERENCES regions;
-
-
