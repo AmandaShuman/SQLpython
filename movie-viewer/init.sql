52205cf5d721bc8fd7af1ccf5534113c756a1583
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-10-06 22:08:18 -0400

    changind ERD to include 1:many tables

--- movie-viewer/init.sql
+++ movie-viewer/init.sql
@@ -7,10 +7,77 @@ CREATE DATABASE movies;
 \c movies
 
 DROP TABLE IF EXISTS trailers;
+DROP TABLE IF EXISTS movies_trailers;
 DROP TABLE IF EXISTS formats;
+DROP TABLE IF EXISTS movies_formats;
 DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS movies_actors;
 DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS movies_genres;
 DROP TABLE IF EXISTS awards;
+DROP TABLE IF EXISTS movies_awards;
 DROP TABLE IF EXISTS directors;
+DROP TABLE IF EXISTS movies_directors;
 DROP TABLE IF EXISTS movies;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS movies_ratings;
+
+--
+-- Name: trailers; Type: TABLE; Schema: public; Owner: Amanda_Shuman; Tablespace: 
+--
+CREATE TABLE trailers (
+    trailer_id SERIAL PRIMARY KEY,
+    trailer_url TEXT NOT NULL
+);
+
+--
+-- Name: movies_trailers; Type: TABLE; Schema: public; Owner: Amanda_Shuman; Tablespace: 
+--
+CREATE TABLE movies_trailers (
+    trailer_id INT NOT NULL,
+    movie_id INT NOT NULL,
+    PRIMARY KEY(trailer_id, movie_id),
+    CONSTRAINT fk_trailer FOREIGN KEY (trailer_id) REFERENCES trailers(trailer_id),
+    CONSTRAINT fk_movie FOREIGN KEY (movie_id) REFERENCES movies(movie_id)
+);
+--
+-- Name: formats; Type: TABLE; Schema: public; Owner: Amanda_Shuman; Tablespace: 
+--
+CREATE TABLE formats (
+    format_id SERIAL PRIMARY KEY,
+    extension TEXT NOT NULL,
+)
+
+--
+-- Name: actors; Type: TABLE; Schema: public; Owner: Amanda_Shuman; Tablespace: 
+--
+
+
+--
+-- Name: genres; Type: TABLE; Schema: public; Owner: Amanda_Shuman; Tablespace: 
+--
+
+
+--
+-- Name: awards; Type: TABLE; Schema: public; Owner: Amanda_Shuman; Tablespace: 
+--
+
+
+--
+-- Name: directors; Type: TABLE; Schema: public; Owner: Amanda_Shuman; Tablespace: 
+--
+
+
+--
+-- Name: movies; Type: TABLE; Schema: public; Owner: Amanda_Shuman; Tablespace: 
+--
+CREATE TABLE movies (
+    movie_id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    year INT NOT NULL,
+    length TEXT NOT NULL
+);
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: Amanda_Shuman; Tablespace: 
+--
